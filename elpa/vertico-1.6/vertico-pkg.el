68422ccc38de1852d1a404cdcb14542fbc99448b
ttn <thomasnguyen729@gmail.com>
2024-01-09 20:19:46 -0600

    Install vertico

---
+++ elpa/vertico-1.6/vertico-pkg.el
@@ -0,0 +1,2 @@
+;; Generated package description from vertico.el  -*- no-byte-compile: t -*-
+(define-package "vertico" "1.6" "VERTical Interactive COmpletion" '((emacs "27.1") (compat "29.1.4.4")) :commit "f7aeda39df860ba1d82d0d94396f8bdeb13c4c8c" :authors '(("Daniel Mendler" . "mail@daniel-mendler.de")) :maintainer '("Daniel Mendler" . "mail@daniel-mendler.de") :keywords '("convenience" "files" "matching" "completion") :url "https://github.com/minad/vertico")
