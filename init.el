9503f04e3eb31384a249d73a734ef8c12813908b
ttn <thomasnguyen729@gmail.com>
2024-01-09 20:21:01 -0600

    Install marginalia

--- init.el
+++ init.el
@@ -8,6 +8,9 @@
 ;; vertico
 (vertico-mode)
 
+;; marginalia
+(marginalia-mode)
+
 
 ;; emacs automatically created
 (custom-set-variables
@@ -15,7 +18,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(vertico magit)))
+ '(package-selected-packages '(marginalia vertico magit)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
