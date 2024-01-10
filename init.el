9a3c8515bbede1006afce3c5ae1d1359252852c9
ttn <thomasnguyen729@gmail.com>
2024-01-09 20:14:58 -0600

    Install magit

--- init.el
+++ init.el
@@ -4,3 +4,18 @@
 ;; appearance
 (set-face-background 'mode-line "plum")
 (set-face-background 'mode-line-inactive "sky blue")
+
+
+;; emacs automatically created
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(magit)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
